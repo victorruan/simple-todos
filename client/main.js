7c8cf8d47b112e2474bb27de8c844fd9be40b88d
阮佳佳 <250069802@qq.com>
2016-07-29 18:59:35 +0800

    ~

--- client/main.js
+++ client/main.js
@@ -2,4 +2,4 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
 
-angular.module('simple-todos',[angularMeteor,todosList.text]);
+angular.module('simple-todos',[angularMeteor,todosList.name]);
